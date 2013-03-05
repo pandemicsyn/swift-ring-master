6e52bd9eedc128c945ff1ddebdb875796387ed19
Florian Hines <syn@ronin.io>
2013-03-05 11:55:28 -0800

    version bump

--- srm/__init__.py
+++ srm/__init__.py
@@ -2,7 +2,7 @@ import gettext
 
 
 #: Version information (major, minor, revision[, 'dev']).
-version_info = (0, 1, 6)
+version_info = (0, 1, 7)
 #: Version string 'major.minor.revision'.
 version = __version__ = ".".join(map(str, version_info))
 gettext.install('swift-ring-master')
